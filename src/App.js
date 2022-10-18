4ff1ed0865ad403ef56b2176845f57bd943a770f
Sandipan <sandipankhanra@gmail.com>
2022-10-18 21:14:17 +0530

    Add Card,Button components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import "./components/ToDoItem";
 import ToDoItem from "./components/ToDoItem";
+import AddTodo from "./components/Todos/AddTodo";
 
 function App() {
-  const toDos = [
-    {
-      title: "Checkout new jobs",
-      date: new Date(2022, 5, 23),
-      priority: "High",
-    },
-    { title: "Practice DSA", date: new Date(2022, 5, 24), priority: "High" },
-    {
-      title: "Complete Udemy courses on React",
-      date: new Date(2022, 5, 23),
-      priority: "Medium",
-    },
-    {
-      title: "Practice linux commands",
-      date: new Date(2022, 6, 23),
-      priority: "Medium",
-    },
-  ];
   return (
     <div>
-      <ToDoItem
-        title={toDos[0].title}
-        date={toDos[0].date}
-        priority={toDos[0].priority}
-      />
-      <ToDoItem
-        title={toDos[1].title}
-        date={toDos[1].date}
-        priority={toDos[1].priority}
-      />
-      <ToDoItem
-        title={toDos[2].title}
-        date={toDos[2].date}
-        priority={toDos[2].priority}
-      />
-      <ToDoItem
-        title={toDos[3].title}
-        date={toDos[3].date}
-        priority={toDos[3].priority}
-      />
+      <AddTodo />
     </div>
   );
 }
--- src/components/Todos/AddTodo.js
+++ src/components/Todos/AddTodo.js
@@ -1,3 +1,29 @@
-const AddTodo = (props) => {};
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddTodo.module.css";
+
+const AddTodo = (props) => {
+  const addToDoHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addToDoHandler}>
+        <label htmlFor="todoTitle">ToDo Title</label>
+        <input id="todoTitle" type="text" />
+        <label htmlFor="priority">Priority</label>
+        <select name="priority" id="priority">
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+          <option value="highest">Highest</option>
+        </select>
+        <label htmlFor="date">Date</label>
+        <input id="date" type="date" />
+        <Button type="submit">Create ToDo</Button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddTodo;
